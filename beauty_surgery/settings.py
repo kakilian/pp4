53f5464beff6edd04e4999c16be56cf35f728b81
Katarina Kilian <khkilian22@gmail.com>
2024-12-02 18:26:47 +0000

    Created eny.py, created Procfile, Updated Secret Django Key

--- beauty_surgery/settings.py
+++ beauty_surgery/settings.py
@@ -20,8 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-2)p)-#ziw%g$%&sg4hjh=sx(%zh8u%7mi!4yi=4^brm%-&v%3)"
-
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "default-secret-key")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
---
+++ eny.py
@@ -0,0 +1,9 @@
+import os
+
+os.environ.setdefault(
+    "DATABASE_URL",
+    "postgresql://neondb_owner:91SCPTsWfpbM@ep-small-truth-a223y76w.eu-central-1.aws.neon.tech/jog_morse_nap_287495",
+)
+os.environ.setdefault["DJANGO_SECRET_KEY"] = (
+    "django-insecure-2)p)-#ziw%g$%&sg4hjh=sx(%zh8u%7mi!4yi=4^brm%-&v%3)"
+)
